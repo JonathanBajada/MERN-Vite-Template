86320b499c859eac8c6e02952fdf798f0e3d7adf
Jonatahn Bajada <jbajada@ryerson.ca>
2023-08-24 18:39:24 -0400

    landing page down in chakra

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,8 +1,10 @@
 import {
 	Box,
 	Image as ChakraImage,
-	Flex,
+	Flex as Navigation,
 	Text as ChakraText,
+	Grid,
+	GridItem,
 	Heading,
 	Container,
 } from "@chakra-ui/react";
@@ -16,45 +18,65 @@ const Landing = () => {
 	return (
 		<Box>
 			{/* nav */}
-			<Flex p="4">
+			<Navigation
+				w="90vw"
+				maxW="1120px"
+				mx="auto"
+				h="6rem"
+				mb="-6rem"
+				alignItems="center">
 				<Box>
 					<ChakraImage src={logo} />
 				</Box>
-			</Flex>
-
-			<Container maxW="container.sm">
-				<Heading>
-					Job{" "}
-					<ChakraText as="span" color="#2cb1bc">
-						Tracking{" "}
-					</ChakraText>
-					App
-				</Heading>
-
-				<ChakraText mt="4">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto
-					deserunt nisi quia dolores, soluta non ea impedit, earum voluptatum
-					eum harum debitis eaque, itaque natus! Quos sapiente laboriosam itaque
-					ipsam.
-				</ChakraText>
-
-				<ButtonGroup spacing="4" mt="4">
-					<Button bg="#2cb1bc">
-						<ChakraLink color="#ffff" as={ReactRouterLink} to="/register">
-							Register
-						</ChakraLink>
-					</Button>
-
-					<Button bg="#2cb1bc">
-						<ChakraLink color="#ffff" as={ReactRouterLink} to="/login">
-							Login / Demo User
-						</ChakraLink>
-					</Button>
-				</ButtonGroup>
-			</Container>
+			</Navigation>
 
 			<Box>
-				<ChakraImage src={main} />
+				<Container w="90vw" maxW="1120px">
+					<Grid
+						alignItems="center"
+						minH="100vh"
+						templateColumns={{ lg: "1fr 400px" }}
+						columnGap="3rem">
+						<GridItem w="100%">
+							<Heading mb="1.5rem">
+								Job{" "}
+								<ChakraText as="span" color="#2cb1bc">
+									Tracking{" "}
+								</ChakraText>
+								App
+							</Heading>
+
+							<ChakraText lineHeight={2} mb={"1.5rem"}>
+								Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+								Architecto deserunt nisi quia dolores, soluta non ea impedit,
+								earum voluptatum eum harum debitis eaque, itaque natus! Quos
+								sapiente laboriosam itaque ipsam.
+							</ChakraText>
+
+							<ButtonGroup spacing="4">
+								<Button bg="#2cb1bc">
+									<ChakraLink color="#ffff" as={ReactRouterLink} to="/register">
+										Register
+									</ChakraLink>
+								</Button>
+
+								<Button bg="#2cb1bc">
+									<ChakraLink color="#ffff" as={ReactRouterLink} to="/login">
+										Login / Demo User
+									</ChakraLink>
+								</Button>
+							</ButtonGroup>
+						</GridItem>
+
+						<GridItem w="100%">
+							<ChakraImage
+								src={main}
+								objectFit="cover"
+								display={{ lg: "block", sm: "none" }}
+							/>
+						</GridItem>
+					</Grid>
+				</Container>
 			</Box>
 		</Box>
 	);
